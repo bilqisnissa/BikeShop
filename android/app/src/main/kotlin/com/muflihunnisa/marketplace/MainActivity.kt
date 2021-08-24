c56b0966b54cd5d7b39422d90dd3ce124dd460f4
Bilqis <hirosasi01@gmail.com>
2021-08-24 20:52:29 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/muflihunnisa/marketplace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muflihunnisa.marketplace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/widget.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+
+final rupiahCurrencyFormat = NumberFormat.simpleCurrency(locale: 'id_ID');
+
+Color orangeColor = Color(0xffD41A7F);
